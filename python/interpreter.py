5875c3703cf0bb05e3fc70f6b1f602b91c1a7651
Splasky <henrychung860326@gmail.com>
2017-08-28 02:57:13 +0800

    [Bugfix] Fix memory usage.

--- python/interpreter.py
+++ python/interpreter.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-# Last modified: 2017-08-27 17:08:46
+# Last modified: 2017-08-27 22:38:10
 import sys
 
 
@@ -27,8 +27,9 @@ class Brainf_VirtualMachine(object):
         self.bracket_map = {}
 
     def __advance(self):
-        self.stack.append(0)
         self.pointer += 1
+        if len(self.stack) <= self.pointer:
+            self.stack.append(0)
 
     def __devance(self):
         self.pointer -= 1
@@ -54,21 +55,17 @@ class Brainf_VirtualMachine(object):
             self.pc = self.bracket_map.get(self.pc)
 
     def program_exec(self, program):
-        try:
-            self.pc = 0
-            self.bracket_map, self.program = self.parse(program)
-            while self.pc < len(self.program):
-                char = self.program[self.pc]
-                if char in self.__instructions:
-                    self.__instructions[char]()
-                self.pc += 1
-
-        except Exception as e:
-            print(e)
-            sys.exit(1)
+        self.pc = 0
+        self.bracket_map, self.program = self.parse(program)
+        while self.pc < len(self.program):
+            char = self.program[self.pc]
+            if char in self.__instructions:
+                self.__instructions[char]()
+            self.pc += 1
 
     def parse(self, program: list):
-        program_list = [x for x in program if x in Brainf_VirtualMachine.ByteCode]
+        program_list = [
+            x for x in program if x in Brainf_VirtualMachine.ByteCode]
 
         pc = 0
         leftstack = []
@@ -90,13 +87,23 @@ class Brainf_VirtualMachine(object):
 def run(argv):
     if len(argv) < 1:
         print("Please supply a filename")
-        sys.exit(1)
+        return 1
 
     with open(argv[1], 'r') as f:
         program = f.read()
 
     vm = Brainf_VirtualMachine()
     vm.program_exec(list(program))
+    return 0
+
+
+def entry_point(argv):
+    run(argv)
+    return 0
+
+
+def target(*args):
+    return entry_point
 
 
 if __name__ == "__main__":
